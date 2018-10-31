1ebd9a0bbaa75b9cd87b302d9556af3ef047c2a8
qinxh12138 <183947317@qq.com>
2018-10-31 20:24:52 +0800

    2

--- car.py
+++ car.py
@@ -1,30 +1,40 @@
 class Vehicles:
-    def __init__(self,brand,color):
-        self.brand=brand
-        self.color=color
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
     def run(self):
         print(f'车子的牌子是:{self.brand}')
         print(f'车子的颜色是:{self.color}')
         print('我已开动了')
 
+
 class Car(Vehicles):
-    seats=4
-    def __init__(self,brand,color):
-        super().__init__(brand,color)
+    seats = 4
+
+    def __init__(self, brand, color):
+        super().__init__(brand, color)
         self.run()
+
     @classmethod
     def show_car(cls):
         print(f'此小汽车载量{cls.seats}人')
+
+
 class Truck(Vehicles):
-    load=8.6
-    def __init__(self,brand, color):
+    load = 8.6
+
+    def __init__(self, brand, color):
         super().__init__(brand, color)
         self.run()
+
     @classmethod
     def show_truck(cls):
         print(f'此卡车载重{cls.load}吨')
 
-c1=Car('奔驰','黄色')
-Car.show_car()
-t1=Car('解放','军绿色')
-Truck.show_truck()
+
+if __name__ == '__main__':
+    c1 = Car('奔驰', '黄色')
+    Car.show_car()
+    t1 = Car('解放', '军绿色')
+    Truck.show_truck()
--- 用户认证.py
+++ 用户认证.py
@@ -1,8 +1,21 @@
-import  time
-def zsq(f):
-    def fun(*args):
-        a=time.time()
-        f(*args)
-        b=time.time()
-        print(b-a)
-    return fun
+users = {
+    'aaa':'111',
+    'bbb':'222'
+}
+def isUser(f):
+    def wrap(*args,**kwargs):
+        name = input('请输入用户名:')
+        passwd = input('请输入密码:')
+        for user in users.items():
+            if user[0]==name and passwd==user[1]:
+                print('认证成功')
+                f()
+                return
+            else:
+                print('认证失败')
+    return wrap
+
+@isUser
+def test():
+    print('认证完成,谢谢!')
+test()
